f21495eda9863e18fd40bc894cf11f32f1ae378d
Daniel Novikov <dnovikov@optonline.net>
2024-08-02 19:05:04 -0400

    removed pycache files

--- Financial Data Structures/imbalance_bars.py
+++ Financial Data Structures/imbalance_bars.py
@@ -4,6 +4,79 @@ from utils.classes.Prices import Prices
 from utils.algorithms.ewma import ewma
 
 
+def tick_imbalance_bars(data: pd.DataFrame, alpha: float, et_init=100) -> pd.DataFrame:
+    """Get tick imbalance bars for a price series
+
+    Implementation described in AFML 2.3.2.1 "Tick Imbalance Bars"
+
+    Args:
+        data: a dataframe containing "price" and "volume" columns
+        alpha: decay factor for EWMA
+
+    Returns:
+        A dataframe containing tick imbalance bars, which recast the price
+        series into bars sized by information content.
+    """
+
+    def make_bar(start_idx, end_idx, data, bars):
+        T = end_idx - start_idx
+        bars["start_idx"].append(start_idx)
+        bars["end_idx"].append(end_idx)
+        bars["open"].append(data["price"][start_idx])
+        bars["high"].append(max(data["price"][start_idx:end_idx]) if T > 0 else data["price"][end_idx])
+        bars["low"].append(min(data["price"][start_idx:end_idx]) if T > 0 else data["price"][end_idx])
+        bars["close"].append(data["price"][end_idx])
+        bars["volume"].append(sum(data["volume"][start_idx:end_idx]) if T > 0 else data["volume"][end_idx])
+
+    def compute_bar_pb(b_hist):
+        bar_pb = 0
+        for b in b_hist:
+            if b == 1:
+                bar_pb += 1
+        bar_pb /= len(b_hist)
+        return bar_pb
+
+    bars = {
+        "start_idx": [0],
+        "end_idx": [1],
+        "open": [data["price"][0]],
+        "high": [data["price"][0]],
+        "low": [data["price"][0]],
+        "close": [data["price"][0]],
+        "volume": [data["volume"][0]],
+    }
+
+    p = data["price"]
+    b = 1
+    b_last = 1
+    b_hist = []
+    theta = 0
+    et = et_init
+    pb = 1
+
+    for t in range(1, len(p)):
+        dp = p[t] - p[t - 1]
+        if dp == 0:
+            b = b_last
+        else:
+            b = 1 if dp > 0 else -1
+            b_last = b
+        theta += b
+        b_hist.append(b)
+
+        if abs(theta) >= et * abs(2 * pb - 1):
+            T = t - bars["end_idx"][-1]
+            make_bar(bars["end_idx"][-1], t, data, bars)
+            et = alpha * T + (1 - alpha) * et
+            bar_pb = compute_bar_pb(b_hist)
+            pb = alpha * bar_pb + (1 - alpha) * pb
+
+            b_hist = []
+            theta = 0
+
+    return pd.DataFrame(bars)
+
+
 def volume_imbalance_bars(data: pd.DataFrame, alpha: float) -> pd.DataFrame:
     """Get volume imbalance bars for a price series
 
@@ -83,79 +156,6 @@ def volume_imbalance_bars(data: pd.DataFrame, alpha: float) -> pd.DataFrame:
             # reset
             b_hist = []
             v_hist = []
-            theta = 0
-
-    return pd.DataFrame(bars)
-
-
-def tick_imbalance_bars(data: pd.DataFrame, alpha: float, et_init=100) -> pd.DataFrame:
-    """Get tick imbalance bars for a price series
-
-    Implementation described in AFML 2.3.2.1 "Tick Imbalance Bars"
-
-    Args:
-        data: a dataframe containing "price" and "volume" columns
-        alpha: decay factor for EWMA
-
-    Returns:
-        A dataframe containing tick imbalance bars, which recast the price
-        series into bars sized by information content.
-    """
-
-    def make_bar(start_idx, end_idx, data, bars):
-        T = end_idx - start_idx
-        bars["start_idx"].append(start_idx)
-        bars["end_idx"].append(end_idx)
-        bars["open"].append(data["price"][start_idx])
-        bars["high"].append(max(data["price"][start_idx:end_idx]) if T > 0 else data["price"][end_idx])
-        bars["low"].append(min(data["price"][start_idx:end_idx]) if T > 0 else data["price"][end_idx])
-        bars["close"].append(data["price"][end_idx])
-        bars["volume"].append(sum(data["volume"][start_idx:end_idx]) if T > 0 else data["volume"][end_idx])
-
-    def compute_bar_pb(b_hist):
-        bar_pb = 0
-        for b in b_hist:
-            if b == 1:
-                bar_pb += 1
-        bar_pb /= len(b_hist)
-        return bar_pb
-
-    bars = {
-        "start_idx": [0],
-        "end_idx": [1],
-        "open": [data["price"][0]],
-        "high": [data["price"][0]],
-        "low": [data["price"][0]],
-        "close": [data["price"][0]],
-        "volume": [data["volume"][0]],
-    }
-
-    p = data["price"]
-    b = 1
-    b_last = 1
-    b_hist = []
-    theta = 0
-    et = et_init
-    pb = 1
-
-    for t in range(1, len(p)):
-        dp = p[t] - p[t - 1]
-        if dp == 0:
-            b = b_last
-        else:
-            b = 1 if dp > 0 else -1
-            b_last = b
-        theta += b
-        b_hist.append(b)
-
-        if abs(theta) >= et * abs(2 * pb - 1):
-            T = t - bars["end_idx"][-1]
-            make_bar(bars["end_idx"][-1], t, data, bars)
-            et = alpha * T + (1 - alpha) * et
-            bar_pb = compute_bar_pb(b_hist)
-            pb = alpha * bar_pb + (1 - alpha) * pb
-
-            b_hist = []
             theta = 0
 
     return pd.DataFrame(bars)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Financial-Machine-Learning
 
-This repo contains implementations of concepts from the book Advances in Financial Machine Learning by Dr. Marcos Lopez de Prada. It also contains other quantitative finance ideas and techniques I come across, such as automated intrinsic value estimation of companies, and statistical arbitrage on spreads between cointegrated pairs of stock. 
+This repo contains implementations of the book Advances in Financial Machine Learning by Dr. Marcos Lopez de Prada, along with other quantitative finance ideas and techniques I come across, like automated intrinsic value estimation of companies, and statistical arbitrage on spreads of cointegrated pairs of stock. 
 
 Contents:
 - Financial Data Structures (AFML Chapter 2)
